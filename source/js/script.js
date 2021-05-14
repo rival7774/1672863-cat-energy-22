dfb3f0969d52fbe1a2c6ec74397b0e3344e6847a
Samir <sakitov@yandex.ru>
2021-05-15 01:21:22 +0400

    Сделал кнопку меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+if (document.querySelector(".main-menu")) {
+  const header = document.querySelector(".page-header");
+  const menu = document.querySelector(".main-menu");
+  const menuButton = document.querySelector(".button-menu");
+  const menuButtonSpan = document.querySelector(".button-menu__item");
+
+  if (header.classList.contains("main-menu--no-js")) {
+    header.classList.remove("main-menu--no-js");
+  }
+
+  menuButton.addEventListener("click", () => {
+    menuButtonSpan.classList.toggle("button-menu__item--active");
+    menu.classList.toggle("main-menu--active");
+  });
+}
